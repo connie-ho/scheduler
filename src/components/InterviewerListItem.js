7f0a6205fac9c4d72d36c787d46f8d294da59080
Connie Ho <hconnie09@gmail.com>
2021-03-15 20:46:28 -0600

    add interview components

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -4,14 +4,23 @@ import './InterviewerListItem.scss';
 
 export default function InterviewerListItem(props) {
 
+  const InterviewerListItemClass = classnames("interviewers__item", {
+    "interviewers__item--selected": props.selected
+  });
+
+
   return(
-    <li key={props.id} className="interviewers__item">
+    <li 
+    key={props.id} 
+    className={InterviewerListItemClass}
+    onClick={props.setInterviewer}
+    >
       <img
         className="interviewers__item-image"
         src={props.avatar}
         alt={props.name}
       />
-      Sylvia Palmer
+      {props.selected && props.name}
     </li>
 
   );
