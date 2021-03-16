d65bd43ca29cc4f5e41db82fc598b9492de39e82
Connie Ho <hconnie09@gmail.com>
2021-03-15 18:06:32 -0600

    add button component

---
+++ src/components/DayListItem.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import classnames from 'classnames';
+import './DayListItem.scss';
+
+export default function DayListItem(props) {
+  
+  const dayListItemClass = classnames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": !props.spots,
+  });
+
+  const formatSpots = function(numSpots) {
+    if (!numSpots) {
+      return 'no spots remaining'
+    }
+
+    if (numSpots === 1) {
+      return '1 spot remaining'
+    }
+
+    return `${numSpots} spots remaining`
+  }
+
+
+  return (
+    <li 
+    className={dayListItemClass}
+    onClick={() => props.setDay(props.name)}
+    >
+      <h2 className="text--regular">{props.name}</h2> 
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
+    </li>
+  );
+}
